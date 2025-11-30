140d9215f242914bdef1629bfc1e30917b4890cc
ferney1234567 <ariasfrancoferney2@gmail.com>
2025-11-30 11:40:26 -0500

    actualizar

--- src/app/components/consumoAgua/page.tsx
+++ src/app/components/consumoAgua/page.tsx
@@ -64,7 +64,7 @@ export default function ConsumoAgua({ modoNoche }: Props) {
 
   // ======= EXPORTAR A EXCEL =======
   const exportarExcel = (): void => {
-    const data: any[] = [];
+   const data: Record<string, string | number>[] = [];
 
     for (let d = 1; d <= totalDias; d++) {
       data.push({
